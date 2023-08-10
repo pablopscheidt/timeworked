d7dbfa01588fc21220ece8dcb7efa7ff970b21fa
Pablo Boing Pscheidt <nikebryt@gmail.com>
2023-08-09 22:38:00 -0300

    Correção import icone

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Checkbox } from './components/Checkbox';
 import { Input } from './components/Input';
 import { Menu } from './components/Menu';
 import { Subtitle } from './components/Subtitle';
+import gitIcon from './assets/github-icon.svg';
 
 function App() {
   function teste(){
@@ -14,7 +15,7 @@ function App() {
       <div className='bg-gray-800 p-8 rounded-lg text-gray-50 flex flex-col gap-10'>
         <div className='flex items-center justify-between gap-10'>
           <a href='https://github.com/pablopscheidt/timeworked' target='_blank' className='bg-gradient-hovered p-1 flex justify-center items-center rounded'>
-            <img src="src/assets/github-icon.svg" alt="Github" width={32} height={32} />
+            <img src={gitIcon} alt="Github" width={32} height={32} />
           </a>
           <h1>
             <img src="/logo.svg" alt="TIMEWORKED" width={340} height={40} />
